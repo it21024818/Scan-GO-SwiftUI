94e59863542ed29f85fe8a9959a4955e4bc93851
“Dinuka <it21024818@my.sliit.lk>
2024-06-12 02:36:29 +0530

    Initial commit

---
+++ code/Scan&Go 2.0/Scan&GoWatch Watch App/Scan_GoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_GoWatchApp.swift
+//  Scan&GoWatch Watch App
+//
+//  Created by Dinuka Dissanayake on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Scan_GoWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
